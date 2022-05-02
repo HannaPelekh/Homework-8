6e53d4b33ae9612345ccbc03359ca848ac44da6c
Hanna <po4taotpravka@gmail.com>
2022-05-02 18:00:25 +0300

    feature/HW-10: added homework-10

--- app.js
+++ app.js
@@ -4,21 +4,41 @@ const toDoListE = document.getElementById('todo-list');
 const containerE = document.getElementById('container');
 
 buttonE.addEventListener('click', onAddToDo);
+toDoListE.addEventListener('click', onToDoClic);
 
 function onAddToDo() {
   const text = inputE.value;
-  const error = 'Please add some text to todo'
-  if(!text.trim()) {
+  const element = `<div name = "todo" class="items">
+  <span name = "delete" class="delete"><i class="fa-solid fa-xmark"></i></span>
+  ${text} </div>`;
+  const error = 'Please add some text to todo'  
+  if(!text.trim() || !text.trim().lenght>3) {
     let div = document.createElement('div');
     div.className = "alert";
     div.innerHTML = error;
     containerE.append(div);
-    setTimeout(() => div.remove(),1000);
+    setTimeout(() => div.remove(),2000);
     return;
   }
-  const item = document.createElement('li');
-  item.textContent = text;
-  toDoListE.append(item);
+  toDoListE.innerHTML += element;
   inputE.value = '';
-  inputE.focus();
+  inputE.focus();  
+}
+function onToDoClic(e){
+  [...e.target.attributes].forEach((el) => {
+    if(el.value === 'delete'){
+      onDelete(e.target);
+    }
+    console.log(e)
+    if(el.value === 'todo'){
+      compliteToDo (e.target);
+    }
+  });
+  
+}
+function compliteToDo (elem){
+  elem.classList.toggle('green');
+}
+function onDelete(elem){  
+  elem.closest('.items').remove();
 }
